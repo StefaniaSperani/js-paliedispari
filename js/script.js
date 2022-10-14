268ec24751f4fbad3024d5533961c38e04fda5dd
StefaniaSperani <thedarkcry@hotmail.it>
2022-10-14 15:19:28 +0200

    creating function

--- js/script.js
+++ js/script.js
@@ -3,6 +3,33 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 
 
+//creo le variabili che prendono gli elementi in pagina
+const parolaInput = document.querySelector('input');
+const btnPali = document.getElementById('btn1');
+
+// creo una funzione che mi dice se la parola inserita dall'utente è palindroma
+btnPali.addEventListener('click', function(){
+    // creo una variabile che prenda la misura della parola
+    const parolaLen = parolaInput.value;
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
